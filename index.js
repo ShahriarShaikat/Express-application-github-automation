8341eaacc32e45c3189829f82c24db5c3dbb0e2e
ShahriarShaikat <shahriarshaikat4@gmail.com>
2026-08-06 00:25:39 +0600

    Update response message in root route to include additional text

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT ?? 8080;
 
 app.get("/", (req, res) => {
   return res.json({
-    msg: "This is a simple Express server.......................!!",
+    msg: "This is a simple Express server.......................!!  sdjkhfuisdhukjfhdsjfjhds",
   });
 });
 
